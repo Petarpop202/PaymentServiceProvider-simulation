ab015ce6fe0126fce3f0eb4d17b378820989d5eb
Petar Popovic <93181074+Petarpop202@users.noreply.github.com>
2023-11-09 16:31:46 +0100

    Spring app init

---
+++ PaymentAPP/src/main/java/org/example/PaymentApplication.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
