6a57dfa85537f6e8fa36b8a49f91843d30f22330
Petar Popovic <petarpop202@gmail.com>
2023-11-11 16:59:00 +0100

    Services pingged successful

--- PaymentAPP/PaymentServiceProvider/src/main/java/org/example/PspApplication.java
+++ PaymentAPP/PaymentServiceProvider/src/main/java/org/example/PspApplication.java
@@ -1,9 +1,15 @@
 package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PspApplication {
     public static void main(String[] args) {SpringApplication.run(PspApplication.class, args);
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
